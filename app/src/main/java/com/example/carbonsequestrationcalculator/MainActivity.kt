50a554f7ff1b42bfe727006d0fe3c4272b4ff42b
Chelsea <cinthavong@gmail.com>
2023-10-09 23:30:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/carbonsequestrationcalculator/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.carbonsequestrationcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val button = findViewById<TextView>(R.id.button
+//        button.setOnClickListener { it: View!
+//            showResult()
+    //     )
+        }
+
+//    private fun calculateResult(): Double {
+//        val canopyheightText = findViewById<EditText>(R.id.canopyHeight)
+//        val canopywidthText = findViewById<EditText>(R.id.canopyWidth)
+//
+//        val canopyHeight = canopyHeightText.text.toString().toDouble()
+//        val canopyWidth = canopyWidthText.text.toString().toDouble()
+//
+//        val dbH: Double = dbH.sqrt((canopyHeight)^2 + (canopyWidth)^2))
+    }
+//    private fun showResult() {
+//        val abgResult = calculateResult()
+//
+//        val resultText = findViewById<TextView>(R.id.result)
+//        val resultText = ("%.2f".format(abgResult))
+//
+//        abgText.text = resultText
+//    }
+
